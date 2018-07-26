2005fb10c2d819f4125f1c90e4039922f5fde015
Vadim Lobanov <vadimplobanov@gmail.com>
2018-07-26 14:33:25 -0700

    Correctly check if symlinks that have a bad target are present, so we can remove those also.

--- ddslib/setup.sh
+++ ddslib/setup.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-if [[ -e lib1 ]]; then rm lib1; fi
-if [[ -e lib2 ]]; then rm lib2; fi
+if [[ -e lib1 || -h lib1 ]]; then rm -f lib1; fi
+if [[ -e lib2 || -h lib2 ]]; then rm -f lib2; fi
 
 ln -s $1/repos/mynewt-dds-micro/libs/dds/src            lib1
 ln -s $1/repos/mynewt-dds-micro/apps/dds_mgr/src/common lib2
